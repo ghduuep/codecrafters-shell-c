cb752a3eb3430d8cd157aafc0f11866d71145468
Guilherme Lima <ghduep@outlook.com>
2025-12-07 18:33:24 -0300

    first attempt

--- src/main.c
+++ src/main.c
@@ -6,6 +6,8 @@ int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
 
+  char commands[][10] = {"exit", "echo"};
+  
   while(1) {
     // TODO: Uncomment the code below to pass the first stage
     printf("$ ");
@@ -29,6 +31,26 @@ int main(int argc, char *argv[]) {
 
       printf("\n");
       continue;
+   }
+
+   if(strcmp(token, "type") == 0) {
+     char *linha = strtok(NULL, "");
+     if (linha != NULL) {
+       int encontrou = 0;
+       for(int i = 0; i < 2; i++) {
+         if(strcmp(commands[i], linha) == 0) {
+           printf("%s is a shell builtin\n", linha);
+           encontrou = 1;
+           break;
+         }
+        }
+
+        if(encontrou == 0) {
+          printf("%s: not found\n", linha);
+         }
+
+         continue;
+      }
    }
 
     printf("%s: command not found\n", command);     
